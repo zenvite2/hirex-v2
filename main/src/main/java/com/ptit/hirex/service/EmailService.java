d97c8381c711567debf4ccf1020248ee2fba8531
viet.tq <trviet4302@gmail.com>
2024-10-23 21:54:57 +0700

    feat: sending html mail

--- main/src/main/java/com/ptit/hirex/service/EmailService.java
+++ main/src/main/java/com/ptit/hirex/service/EmailService.java
@@ -1,8 +1,11 @@
 package com.ptit.hirex.service;
 
+import jakarta.mail.MessagingException;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface EmailService {
     boolean sendMail(String to, String subject, String text);
+
+    boolean sendHtmlEmail(String to, String subject, String htmlBody) throws MessagingException;
 }
--- main/src/main/java/com/ptit/hirex/service/impl/EmailServiceImpl.java
+++ main/src/main/java/com/ptit/hirex/service/impl/EmailServiceImpl.java
@@ -1,33 +1,37 @@
 package com.ptit.hirex.service.impl;
 
 import com.ptit.hirex.service.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class EmailServiceImpl implements EmailService {
 
-    private final JavaMailSender emailSender;
+    private final JavaMailSender mailSender;
     private final String sender;
 
-    public EmailServiceImpl(JavaMailSender emailSender, @Value("${spring.mail.from}") String sender) {
-        this.emailSender = emailSender;
+    public EmailServiceImpl(JavaMailSender mailSender, @Value("${spring.mail.from}") String sender) {
+        this.mailSender = mailSender;
         this.sender = sender;
     }
 
+    @Override
     public boolean sendMail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(sender);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
-        message.setFrom(sender);
 
         try {
-            emailSender.send(message);
+            mailSender.send(message);
             return true;
         } catch (Exception e) {
             log.error(e.getMessage(), e);
@@ -35,4 +39,22 @@ public class EmailServiceImpl implements EmailService {
         }
     }
 
+    @Override
+    public boolean sendHtmlEmail(String to, String subject, String htmlBody) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+        helper.setFrom(sender);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlBody, true);
+
+        try {
+            mailSender.send(mimeMessage);
+            return true;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return false;
+        }
+    }
 }
