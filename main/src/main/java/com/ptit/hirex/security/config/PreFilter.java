2c5f061b18214d4df39231f411bee54d67e4d895
viet.tq <trviet4302@gmail.com>
2024-10-27 14:52:51 +0700

    fix: trace request url log

--- main/src/main/java/com/ptit/hirex/security/config/PreFilter.java
+++ main/src/main/java/com/ptit/hirex/security/config/PreFilter.java
@@ -34,7 +34,7 @@ public class PreFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         try {
-            log.info("---------- doFilterInternal ----------");
+            log.info("\n---------- doFilterInternal ----------\n" + "Request URL: {}", request.getRequestURL());
 
             final String authorization = request.getHeader(AUTHORIZATION);
             //log.info("Authorization: {}", authorization);
