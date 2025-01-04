22b48f5036b8b556a33313be831554da6d60428f
duong.dd <duong.dd@tinasoft.com.vn>
2025-01-04 15:32:52 +0700

    fix follow company

--- data/src/main/java/com/ptit/data/repository/FollowCompanyRepository.java
+++ data/src/main/java/com/ptit/data/repository/FollowCompanyRepository.java
@@ -2,12 +2,14 @@ package com.ptit.data.repository;
 
 import com.ptit.data.entity.FollowCompany;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface FollowCompanyRepository extends JpaRepository<FollowCompany, Long> {
+    @Query("SELECT c from FollowCompany c where c.employeeId = :id")
     List<FollowCompany> findAllByEmployeeId(Long id);
 
     void deleteByEmployeeIdAndCompanyId(Long id, Long companyId);
